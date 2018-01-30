b67a70d9ef02b639419d249a0bc0aa773c9b4751
HaichuanQD <xinghaichuan8911@gmail.com>
2018-01-31 00:31:32 +0800

    添加其他网页文件

---
+++ js/swiper.animate.param.js
@@ -0,0 +1,31 @@
+// 主swiper对象的动画参数
+var swiperAnimateParam_main1 = {
+	
+	// 第 1 张幻灯片
+	slide_1:{
+		animate_01:{element:'main1_ani_01', animation:'fadeInDown',delay:'0.1s',duration : '0.7s',},
+		animate_02:{element:'main1_ani_02', animation:'fadeIn',delay:'0.4s',duration : '0.9s',},
+		animate_03:{element:'main1_ani_03', animation:'fadeInUp',delay:'0.4s',duration : '0.9s',},
+		animate_04:{element:'main1_ani_04', animation:'tada',delay:'1s',type : 1,},
+	},
+	
+	// 第 2 张幻灯片
+	slide_2:{
+		// 在此为幻灯片中的动画元素进行动画设置
+	},
+	
+	// 第 3 张幻灯片
+	slide_3:{
+		// 在此为幻灯片中的动画元素进行动画设置
+	},
+	
+	// 第 4 张幻灯片
+	slide_4:{
+		// 在此为幻灯片中的动画元素进行动画设置
+	},
+	
+	// 第 5 张幻灯片
+	
+	
+};
+
