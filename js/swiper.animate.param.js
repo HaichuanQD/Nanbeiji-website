1a9b6807fb310db190c6150464ca4c1691763c54
HaichuanQD <xinghaichuan8911@gmail.com>
2018-01-31 17:17:05 +0800

    制作轮播图第二张完毕

--- js/swiper.animate.param.js
+++ js/swiper.animate.param.js
@@ -4,14 +4,17 @@ var swiperAnimateParam_main1 = {
 	// 第 1 张幻灯片
 	slide_1:{
 		animate_01:{element:'main1_ani_01', animation:'fadeInDown',delay:'0.1s',duration : '0.7s',},
-		animate_02:{element:'main1_ani_02', animation:'fadeIn',delay:'0.4s',duration : '0.9s',},
+		animate_02:{element:'main1_ani_02', animation:'fadeIn',delay:'0.4s',duration : '0.9s',timing:'ease-in',},
 		animate_03:{element:'main1_ani_03', animation:'fadeInUp',delay:'0.4s',duration : '0.9s',},
 		animate_04:{element:'main1_ani_04', animation:'tada',delay:'1s',type : 1,},
 	},
 	
 	// 第 2 张幻灯片
 	slide_2:{
-		// 在此为幻灯片中的动画元素进行动画设置
+		animate_05:{element:'main1_ani_05', animation:'fadeIn',delay:'0.1s',duration : '0.5s',},
+		animate_06:{element:'main1_ani_06', animation:'fadeIn',delay:'1.5s',duration : '2s',},
+		animate_07:{element:'main1_ani_07', animation:'fadeIn',delay:'0.1s',duration : '0.5s',},
+		animate_08:{element:'main1_ani_08', animation:'tada',delay:'1s',type : 1,},
 	},
 	
 	// 第 3 张幻灯片
